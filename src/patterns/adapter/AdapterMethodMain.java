f2be8b6fc4d82e6d52df6cebde8daab9a10521d3
Sergey Shadrin <juuute3110@gmail.com>
2024-11-01 17:39:34 +0700

    Adapter

---
+++ src/patterns/adapter/AdapterMethodMain.java
@@ -0,0 +1,11 @@
+package patterns.adapter;
+
+public class AdapterMethodMain {
+    public static void main(String[] args) {
+        Database database = new AdapterJavaToDatabase();
+        database.insert();
+        database.update();
+        database.select();
+        database.remove();
+    }
+}
