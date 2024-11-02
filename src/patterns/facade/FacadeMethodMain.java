55accf5bae19672e8da6ebd28534f4baf2f1254d
Sergey Shadrin <juuute3110@gmail.com>
2024-11-02 11:24:12 +0700

    facade

---
+++ src/patterns/facade/FacadeMethodMain.java
@@ -0,0 +1,9 @@
+package patterns.facade;
+
+public class FacadeMethodMain {
+    public static void main(String[] args) {
+        Workflow workflow = new Workflow();
+
+        workflow.solveProblems();
+    }
+}
---
+++ src/patterns/facade/Job.java
@@ -0,0 +1,7 @@
+package patterns.facade;
+
+public class Job {
+    public void doJob(){
+        System.out.println("job in progress...");
+    }
+}
